6fe96b7da1efe83182e90eda5ae8f5573619ea7b
Tristan Camejo <contact@tristancamejo.com>
2023-04-15 11:49:08 +1000

    feat: simp club cmd

--- Modules/StaffUtils.cs
+++ Modules/StaffUtils.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Interactions;
 using Microsoft.Extensions.Configuration;
 
@@ -12,6 +13,7 @@ public class StaffUtils : InteractionModuleBase<SocketInteractionContext>
     private readonly IConfiguration _configuration;
 
     private readonly ulong _staffPingRoleId = 1054687518936793118;
+    private readonly ulong _simpClubRoleId = 1058914301252993094;
     private InteractionHandler _handler;
 
     public StaffUtils(InteractionHandler handler, IConfiguration configuration, NatsirtCave cave, AdminAPI client)
@@ -43,6 +45,28 @@ public class StaffUtils : InteractionModuleBase<SocketInteractionContext>
         {
             await member.AddRoleAsync(role);
             await Context.Interaction.FollowupAsync("Staff mode enabled!", ephemeral: true);
+        }
+    }
+    
+    [SlashCommand("toggle-simp", "toggle simp club role")]
+    public async Task ToggleSimpAsync([Summary("target", "the user")]IGuildUser user, [Summary("public-shame", "whether or not to publicly shame the user")]bool publicShame = false)
+    {
+        await Context.Interaction.DeferAsync(!publicShame);
+
+        var role = Context.Guild.GetRole(_simpClubRoleId);
+        var member = Context.Guild.GetUser(user.Id);
+
+        if (member.Roles.Contains(role))
+        {
+            await member.RemoveRoleAsync(role);
+            await Context.Interaction.FollowupAsync(
+                $"banished {user.Mention} from the tristan simp club...",
+                ephemeral: !publicShame);
+        }
+        else
+        {
+            await member.AddRoleAsync(role);
+            await Context.Interaction.FollowupAsync($"{user.Mention} has joined the tristan simp club 😭", ephemeral: !publicShame);
         }
     }
 }
